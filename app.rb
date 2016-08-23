bdfa2df9d6eb9be3ea29bf65dd5638f448bacf22
roofner <steelerbaby210@hotmail.com>
2016-08-23 13:28:42 -0400

    app from class

--- app.rb
+++ app.rb
@@ -1,5 +1,38 @@
 require "sinatra"
 
 get "/" do
-	 "Hello world. How are you today?"
+	erb :get_name
+end
+
+post '/name' do
+  name = params[:user_name]
+  redirect "/age?user_name=" +name
+end
+get "/age" do
+	name = params[:user_name]
+	erb :get_age, :locals => {:your_name => name}
+end
+post "/age" do
+	age = params[:user_age]
+	name = params[:user_name]
+	redirect "/numbers?age=#{age}&user_name=#{name}"
+	# if   age.to_i <60 
+	# 	erb :numbers
+	# else
+	# 	"In ten years you will be: #{params[:user_age].to_i  + 10}."
+	# end
+end
+get "/numbers" do 
+	age = params[:age]
+	name = params[:user_name]
+	erb :numbers, :locals => {:your_age => age, :your_name => name}
+end
+post "/numbers" do
+	fav1 = params[:fav1].to_i
+	fav2 = params[:fav2].to_i
+	fav3 = params[:fav3].to_i
+    sum =  fav1 + fav2 + fav3
+    "The sum of #{fav1}, #{fav2}, #{fav3} is #{sum}"
+
+
 end
